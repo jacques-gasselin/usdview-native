98a4f49b433c8f430a6ca4f815e00bd0aca78481
superfunc <superfunc@users.noreply.github.com>
2018-09-01 23:27:35 -0700

    Cleanup build script, add linter.

--- macos/usdview/DocumentController.swift
+++ macos/usdview/DocumentController.swift
@@ -9,13 +9,12 @@ import Cocoa
 
 class DocumentController: NSDocumentController {
     override func beginOpenPanel(_ openPanel: NSOpenPanel, forTypes inTypes: [String]?, completionHandler: @escaping (Int) -> Void) {
-        
-        var types : [String] = []
-        
+        var types: [String] = []
+
         if let inTypes = inTypes {
-            types.append(contentsOf:inTypes)
+            types.append(contentsOf: inTypes)
         }
-        
+
         // This is temporary until we figure out why the UTIs are not working
         // with the CFDocumentTypes declaration
         if types.count < 1 {
@@ -24,11 +23,11 @@ class DocumentController: NSDocumentController {
             types.append("usdc")
             types.append("usdz")
         }
-        
+
         super.beginOpenPanel(openPanel, forTypes: types, completionHandler: completionHandler)
     }
- 
-    override func documentClass(forType typeName: String) -> AnyClass? {
+
+    override func documentClass(forType _: String) -> AnyClass? {
         return Document.self
     }
 }
